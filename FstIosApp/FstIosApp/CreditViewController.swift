4a015e069d172e1776f839be0e7094d4a5c08884
Seo <kirin2211@naver.com>
2023-07-01 15:47:35 +0900

    creditView

---
+++ FstIosApp/FstIosApp/CreditViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CreditViewController.swift
+//  FstIosApp
+//
+//  Created by Seo on 2023/07/01.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    @IBAction func touchUpDismissButton(_ sender : UIButton){
+        dismiss(animated: true, completion: nil)
+    }
+}
